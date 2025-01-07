9d15a4b7fbb2d7c998419018008f07f7312c08fd
ha3sh <144370932+ha3sh@users.noreply.github.com>
2025-01-07 10:36:54 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversion_datatypes.js
@@ -0,0 +1,30 @@
+let score = "33"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber);
+
+//console.log(score);
+
+//"33" = 33
+//"33abc" = NaN
+//true =1  ; false =0
+
+
+let isLoggedIn = 1
+let boolean = Boolean(isLoggedIn)
+console.log(boolean);
+
+// 1 = true ; 0 = false
+// "" = false
+// "harsh" = true
+
+let number = 33
+
+let stringNumber = String(number)
+console.log(stringNumber);
+
+
