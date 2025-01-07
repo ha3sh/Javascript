89c2b9f43a57f02c0e032d18f18a20a7783bb56d
ha3sh <144370932+ha3sh@users.noreply.github.com>
2025-01-07 18:18:43 +0000

    Operations

--- 01_basics/03_conversion_datatypes.js
+++ 01_basics/03_conversion_datatypes.js
@@ -1,4 +1,4 @@
-let score = "33"
+let score = null
 
 //console.log(typeof score);
 //console.log(typeof (score));
@@ -16,7 +16,7 @@ let valueInNumber = Number(score)
 
 let isLoggedIn = 1
 let boolean = Boolean(isLoggedIn)
-console.log(boolean);
+//console.log(boolean);
 
 // 1 = true ; 0 = false
 // "" = false
@@ -25,6 +25,42 @@ console.log(boolean);
 let number = 33
 
 let stringNumber = String(number)
-console.log(stringNumber);
+//console.log(stringNumber);
 
+/**********************************************OPERATIONS******************************************************** */
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**3);
+//console.log(2/3);
+//console.log(8%2);
+
+let str1 = "Hello"
+let str2 = "  Harsh"
+let str3 = str1 + str2
+//console.log(str3);
+
+//console.log("1" + 2);
+//console.log(1 + "2");
+//console.log("1" + 2 + 2);
+//console.log(1 + 2 + "5");
+//console.log(1 + "2" + 2);
+
+//console.log((2+2) * 3 % 5) ;
+
+//console.log(+true);
+//console.log(+"");
+
+let num1 , num2 ,num3
+num1 = num2 = num3 = 2+2    //Ignore
+//console.log(num3);
+
+let game = 100
+++game ;
+console.log(game);
 
